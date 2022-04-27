906b8e3eb930d48aded517a47e035b6b88d98429
khan <kevin-0503@outlook.com>
2022-04-27 11:00:20 -0500

    Firts Commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,18 @@
+export const getGifs = async ( category ) => {
+    
+    const url =
+      `https://api.giphy.com/v1/gifs/search?api_key=k0T9tuzv21P3JdRQroSUwdxgB1vXZS1H&q=${ encodeURI( category ) }=10&offset=0&rating=g&lang=en`
+
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+    const gifs = data.map(( img ) => {
+      return {
+        id: img.id,
+        title: img.title,
+        url: img.images?.downsized_medium.url,
+      };
+    });
+
+    return gifs;
+  };
